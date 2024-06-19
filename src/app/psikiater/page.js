9f3fe8a5192ae450648dfc19798740a95c57a9d1
adittyapn <adityapn2731@gmail.com>
2024-06-19 15:25:26 +0700

    add api

--- src/app/psikiater/page.js
+++ src/app/psikiater/page.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://voc-server.vercel.app/");
 
 function PsychiatristList() {
   const psychiatrists = ["Psikiater 1", "Psikiater 2", "Psikiater 3"];
